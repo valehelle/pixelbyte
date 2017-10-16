c1aeac892a961036f02b8147343d599a536397d9
hazmi irfan <hazmi@lelong.com.my>
2017-10-16 16:06:37 +0800

    add facebook auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.omniauth :facebook, "APP_ID", "APP_SECRET"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
