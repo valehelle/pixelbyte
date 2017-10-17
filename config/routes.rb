05bf6a8c7fecd5fed3d4926cc87f23a295c4f5a4
hazmi irfan <hazmi@lelong.com.my>
2017-10-17 15:01:58 +0800

    add dashboard to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :webhook
-  resources :pages
+  resources :pages, path: '/dashboard/pages'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
