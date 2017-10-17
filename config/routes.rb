e38b8e6690bf20edc2826e71e57f9cb27f8d2c2d
hazmi irfan <hazmi@lelong.com.my>
2017-10-17 14:52:46 +0800

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :webhook
+  resources :pages
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
