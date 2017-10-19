e4ae2240711744c5712c7b6fd5f51b305f14c339
hazmi irfan <hazmi@lelong.com.my>
2017-10-19 19:04:50 +0800

    fix to page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
-
-  def index
-    @graph = Koala::Facebook::API.new(current_user.access_token)
-    pages = @graph.get_connections('me', 'accounts')
-    pages.each do |page|
-
-      name =  page['name']
-      access_token = page['access_token']
-      page_id = page['id']
-      permission = false
-      perms = page['perms']
-      perms.each do |perm|
-        if perm == 'ADMINISTER'
-          permission = true
-        end
-      end
-
-      page = current_user.pages.where(page_id: page_id).first()
-      if page.nil?
-        puts "empty"
-        page = current_user.pages.build(name: name, access_token: access_token, is_admin: permission, page_id: page_id)
-      else
-        puts "Not empty"
-        page.name = name
-        page.access_token = access_token
-        page.is_admin = permission
-        page.page_id = page_id
-      end
-      page.save
-    
-    end
-  end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,39 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @graph = Koala::Facebook::API.new(current_user.access_token)
+    pages = @graph.get_connections('me', 'accounts')
+    pages.each do |page|
+
+      name =  page['name']
+      access_token = page['access_token']
+      page_id = page['id']
+      permission = false
+      perms = page['perms']
+      perms.each do |perm|
+        if perm == 'ADMINISTER'
+          permission = true
+        end
+      end
+
+      page = current_user.pages.where(page_id: page_id).first()
+      if page.nil?
+        puts "empty"
+        page = current_user.pages.build(name: name, access_token: access_token, is_admin: permission, page_id: page_id)
+      else
+        puts "Not empty"
+        page.name = name
+        page.access_token = access_token
+        page.is_admin = permission
+        page.page_id = page_id
+      end
+      page.save
+     
+    end
+    puts 'asd fsad fsad f sadf fsd'
+    @tab = current_user.pages
+    puts 'asfdsdaf'
+    puts @tab.first().name
+  end
 end
