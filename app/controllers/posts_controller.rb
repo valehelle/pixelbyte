e5c633ba5849ec634785b0bcb7a75c99da5ef99d
hazmi irfan <hazmi@lelong.com.my>
2017-10-21 17:14:24 +0800

    add update to post options

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    before_action :authenticate_user!
     def index
     end
     def show
@@ -10,4 +11,22 @@ class PostsController < ApplicationController
         @post = @current_tab.posts.find_by(id: post_id)
         
     end
+    def update
+        page_id = params[:page_id]
+        post_id = params[:post_id]
+        @current_tab = current_user.pages.find_by(id: page_id)
+        puts @current_tab
+        @post = @current_tab.posts.find_by(id: post_id)
+        
+        if @post.update_attributes(post_params)
+            redirect_to post_path, :notice => "Your post have been updated"
+        else
+            render "show"
+        end
+    end
+
+    private
+        def post_params
+            params.require(:post).permit(:is_private_message, :private_message_content, :is_reply, :reply_content)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :webhook
   resources :pages, path: '/dashboard/page'
   get '/dashboard/page/:page_id/post/:post_id', to: 'posts#show', as: 'post'
+  patch '/dashboard/page/:page_id/post/:post_id', to: 'posts#update', as: 'update_post'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
