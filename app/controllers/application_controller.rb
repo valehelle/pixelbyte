d854cf7f304a4ead68f4512a64dbf236952c6d87
Hazmi Irfan Bin Zainal Ariffin <Peco@Hazmis-Mac-mini.local>
2017-10-29 17:19:41 +0800

    Main title redirect to page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  private 
+
+  def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+  end
+  def after_sign_in_path_for(resource_or_scope)
+      pages_path
+  end
 end
