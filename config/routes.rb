f3678331daafe89da346a7b10153ab6e8d41821a
hazmi irfan <hazmi@lelong.com.my>
2017-11-10 22:37:59 +0800

    add router

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => 'pages#index'
   resources :webhook
   resources :pages, path: '/dashboard/page'
   get '/dashboard/page/:page_id/post/:post_id', to: 'posts#show', as: 'post'
